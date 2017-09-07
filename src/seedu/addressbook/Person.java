d13ed06096b9d204c626051c7f3bd0bbf724c61d
kengying <L.kengying@gmail.com>
2017-09-07 10:37:00 +0800

    add in Person constructor

--- src/seedu/addressbook/Person.java
+++ src/seedu/addressbook/Person.java
@@ -6,5 +6,9 @@ public class Person {
     String phoneNumber;
     String email;
 
-
+    public Person(String name, String phoneNumber, String email) {
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
+    }
 }
